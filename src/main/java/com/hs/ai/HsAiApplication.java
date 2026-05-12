6930e3c2a3e220faf2bace299b7d126fe82e321e
1685432 <hox009@ucsd.edu>
2026-05-11 18:19:37 -0700

    feat: basic spring boot structure with custom package names

---
+++ src/main/java/com/hs/ai/HsAiApplication.java
@@ -0,0 +1,11 @@
+package com.hs.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsAiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HsAiApplication.class, args);
+    }
+}
